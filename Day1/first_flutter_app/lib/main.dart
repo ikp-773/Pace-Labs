dbd7a5858496ba3666c16c669cc57a6607749007
Abhijith Kp <abhijithkp773@gmail.com>
2021-11-12 11:25:21 +0530

    Day 1

---
+++ Day1/first_flutter_app/lib/main.dart
@@ -0,0 +1,39 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const Home(),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My First  App'),
+      ),
+      body: Center(
+        child: Icon(
+          Icons.account_balance_rounded,
+        ),
+      ),
+    );
+  }
+}
